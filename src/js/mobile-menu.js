cdf90b52d7288f8a9576e18820505cd89f680241
DmitriyGovosyants <Demiangvs@gmail.com>
2022-06-08 21:58:00 +0300

    feat: add mobile-menu and js functional

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,26 @@
+const refs = {
+    mobileMenu: document.querySelector('.js-mobile-menu'),
+    openMenuBtn: document.querySelector('.js-mobile-menu-btn-open'),
+    closeMenuBtn: document.querySelector('.js-mobile-menu-btn-close'),
+}
+
+const handleMobileMenuOpen = () => {
+    refs.mobileMenu.classList.add('is-open');
+    refs.openMenuBtn.setAttribute('aria-expanded', true);
+    document.body.style.overflow = "hidden";
+}
+
+const handleMobileMenuClose = () => {
+    refs.mobileMenu.classList.remove('is-open');
+    refs.openMenuBtn.setAttribute('aria-expanded', false);
+    document.body.style.overflow = "";
+}
+
+const handleMobileMenuIfTabletClose = e => {
+    if (!e.matches) return;
+    handleMobileMenuClose()
+}
+
+refs.openMenuBtn.addEventListener('click', handleMobileMenuOpen);
+refs.closeMenuBtn.addEventListener('click', handleMobileMenuClose);
+window.matchMedia('(min-width: 768px)').addEventListener('change', handleMobileMenuIfTabletClose);
