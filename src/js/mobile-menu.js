0c3269f48cce4206e1340de171c5ab4c3550eccb
DmitriyGovosyants <Demiangvs@gmail.com>
2022-06-13 22:53:48 +0300

    feat: close mob menu when click outside

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -9,15 +9,26 @@ const handleMobMenuOpen = () => {
     refs.mobileMenu.classList.add('is-open');
     refs.openMenuBtn.setAttribute('aria-expanded', true);
     document.body.style.overflow = "hidden";
+    document.addEventListener('click', handleOutsideMobMenuClose);
 }
 
 const handleMobMenuClose = () => {
     refs.mobileMenu.classList.remove('is-open');
     refs.openMenuBtn.setAttribute('aria-expanded', false);
     document.body.style.overflow = "";
+    document.removeEventListener('click', handleOutsideMobMenuClose);
 }
 
-const handleMobMenuIfTabletClose = e => {
+const handleOutsideMobMenuClose = (e) => {
+    const targetMobMenu = e.target.closest('.menu');
+    const targetMobMenuOpenBtn = e.target.classList.contains('js-menu-mob-outside-close');
+
+    if (!targetMobMenu && !targetMobMenuOpenBtn) {
+        handleMobMenuClose();
+    }
+}
+
+const handleMobMenuIfDesktopClose = e => {
     if (!e.matches) return;
     handleMobMenuClose();
 }
@@ -29,6 +40,6 @@ const handleMobMenuAnchorTo = e => {
 
 refs.openMenuBtn.addEventListener('click', handleMobMenuOpen);
 refs.closeMenuBtn.addEventListener('click', handleMobMenuClose);
-window.matchMedia('(min-width: 1140px)').addEventListener('change', handleMobMenuIfTabletClose);
+window.matchMedia('(min-width: 1140px)').addEventListener('change', handleMobMenuIfDesktopClose);
 
 refs.mobileMenuNav.addEventListener('click', handleMobMenuAnchorTo);
